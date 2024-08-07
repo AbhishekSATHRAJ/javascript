42a1c7d69f2f2ec40825eedbe85e769adebc46e7
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-08-07 21:24:18 +0530

    java

---
+++ java.js
@@ -0,0 +1,79 @@
+// //
+
+// let x = 11;
+// let y = 11;
+// if (10>=11) {
+//   console.log(x+=y);
+// } 
+// else if (x > y) {
+//   console.log(` 
+// greathan value`);
+// } 
+// else if (x < y) {
+//   console.log(`lessthan value`);
+// }
+//  else  {
+//   console.log(`nothing`);
+// }
+
+
+
+
+
+
+// let x=0;
+//     y=5;
+//     z=10;
+// console.log(x<y?"Largest Number":"Smallest Number")
+
+
+
+
+
+
+
+    //   let a = 30;
+    //   let b = 25;
+    //   let c = 20;
+
+    //   if(a<b && b>a) {
+    //     console.log("b is greatest")
+    //   } 
+    //   else if (a>b && b<a) {
+    //     console.log("a is greatest")
+    //   } 
+    //   else if (c){
+    //     console.log("c is greatest")
+    //   }
+
+
+
+
+
+
+
+
+
+
+
+
+// let opt1=prompt("enter the value")
+// let opt2=prompt("enter the value")
+// let res=opt1+opt2
+// alert(res)
+
+
+
+
+
+
+
+
+let x=3;
+y=-7;
+z=12;
+console.log(x>y && x>z)
+console.log(x+","+y+","+z)
+
+
+
