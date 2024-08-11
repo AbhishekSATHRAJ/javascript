050640b9ab12d28d624ed930bfca1ed8f65ccad1
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-08-11 21:56:08 +0530

    modified

---
+++ index.js
@@ -0,0 +1,74 @@
+// ----> For loop <-----
+
+
+
+for(i=1;i<=10;i++){
+    console.log(h(i**2))
+}
+
+let sum=0;
+for(i=1;i<=20;i++){
+    if(i%3==0)
+   sum=sum+i
+}
+console.log(`the sum of all numbers from 1 to 20 is ${i} = ${sum}`)
+
+
+
+for(i=1;i<=15;i++){
+    console.log(i**3)
+}
+
+// -----> Do while loop <----
+
+do{
+for(i=10;i>=1;i--){
+    console.log(i)
+}}
+while(i)
+
+
+
+
+do{
+    for(i=1;i<=12;i++){
+        let sum=0;
+        sum=sum+i;
+    }
+}while(console.log(sum))
+
+
+
+
+do{
+    for(i=20;i>=1;i--){
+        if(i%2==0)
+        console.log(i)
+    }}
+    while(i)
+
+
+      
+ 
+// -----> while loop <-----
+
+  var i=1;
+  while(i<=20){
+    if(i%5!=0)
+   console.log(i);
+     i++
+  }
+
+  var i=1;
+  var sum=0
+  while(i<=30){
+    if(i%4==0)
+    sum=sum+i
+    i++
+  }console.log(sum)
+
+var i = 50;
+while (i >= 0) {
+  if (i % 3 == 0) console.log(i);
+  i--;
+}
