ce3b3c9f418abdaa6046bfd76840025b41ae5048
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-08-12 21:18:09 +0530

    modified

---
+++ problem.js
@@ -0,0 +1,36 @@
+var a=50;
+    b=40;
+    c=30;
+if(a>b && a>c){
+    console.log("a is largest number");
+}
+else if(b>c){
+    console.log("b is largest number")
+}
+else{
+    console.log("c is largest number")
+}
+
+var a=50;
+    b=40;
+    c=30;
+result=(a>b && a>c)?"a is bigger number":(b>c)?"b is bigger number":"c is bigger number";
+console.log(result)
+
+
+
+for(i=1;!(i>=10+1);i++){
+    console.log(i)
+}
+
+for(e=1;(e!=10+1);e++){
+    console.log(e)
+}
+
+for(i=10;(i>=0);i--){
+    console.log(i)
+}
+
+for(i=10;!(i<=0);i--){
+    console.log(i)
+}
