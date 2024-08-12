18b9973548a6fb8ed5c53ad42d41d33c8bf38513
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-08-12 11:15:45 +0530

    modified

--- index.js
+++ index.js
@@ -1,71 +1,75 @@
 // ----> For loop <-----
 
-
-
-for(i=1;i<=10;i++){
-    console.log(h(i**2))
+for (i = 1; i <= 10; i++) {
+  console.log(h(i ** 2));
 }
 
-let sum=0;
-for(i=1;i<=20;i++){
-    if(i%3==0)
-   sum=sum+i
+
+
+let sum = 0;
+for (i = 1; i <= 20; i++) {
+  if (i % 3 == 0) sum = sum + i;
 }
-console.log(`the sum of all numbers from 1 to 20 is ${i} = ${sum}`)
+console.log(`the sum of all numbers from 1 to 20 is ${i} = ${sum}`);
 
 
 
-for(i=1;i<=15;i++){
-    console.log(i**3)
+for (i = 1; i <= 15; i++) {
+  console.log(i ** 3);
 }
+
+
 
 // -----> Do while loop <----
 
-do{
-for(i=10;i>=1;i--){
-    console.log(i)
-}}
-while(i)
+
+let i = 10;
+do {
+  console.log(i);
+  i--;
+} while (i >= 1);
 
 
 
-
-do{
-    for(i=1;i<=12;i++){
-        let sum=0;
-        sum=sum+i;
-    }
-}while(console.log(sum))
-
-
+let i = 1;
+let sum = 1;
+do {
+  sum = sum * i;
+  i++;
+} while (i <= 12);
+console.log(sum);
 
 
-do{
-    for(i=20;i>=1;i--){
-        if(i%2==0)
-        console.log(i)
-    }}
-    while(i)
+
+let i = 20;
+do {
+  console.log(i);
+  i = i - 2;
+} while (i >= 1);
 
 
-      
- 
+
 // -----> while loop <-----
 
-  var i=1;
-  while(i<=20){
-    if(i%5!=0)
-   console.log(i);
-     i++
-  }
 
-  var i=1;
-  var sum=0
-  while(i<=30){
-    if(i%4==0)
-    sum=sum+i
-    i++
-  }console.log(sum)
+
+var i = 1;
+while (i <= 20) {
+  if (i % 5 != 0) console.log(i);
+  i++;
+}
+
+
+
+var i = 1;
+var sum = 0;
+while (i <= 30) {
+  if (i % 4 == 0) sum = sum + i;
+  i++;
+}
+console.log(sum);
+
+
 
 var i = 50;
 while (i >= 0) {
