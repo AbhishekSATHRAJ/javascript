9ed2b9e44a882cc938f9142deb3192a39cf905e2
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-08-26 19:31:27 +0530

    modified

---
+++ javatest.js
@@ -0,0 +1,66 @@
+// let arr=["hello", "world", "abc"];
+// let arr1=["xyz", "abc", "def"];
+// let result=arr.map((a)=>{
+//     return a.toUpperCase()
+// })
+// let result1=arr1.map((b)=>{
+//     return b.toUpperCase()
+// })
+// console.log(result,result1)
+
+
+
+
+
+// let arr=["hello", "world", "abc"];
+// let x=arr.map((a)=>{
+//     return (a.substring(0,3))
+// })
+// console.log(x)
+
+
+
+
+
+// let arr=["xyz", "foo", "bar"];
+// let x=arr.map((a)=>{
+//     return (a.replaceAll("o",0))
+// })
+// console.log(x)
+
+
+
+
+
+
+// let characters = ["hello", "world", "abc", "defghi"];
+// let x=characters.filter((a) => {
+ 
+//     return (a.length>5);
+  
+// })
+// console.log(x)
+
+
+
+
+// let arr=["hello", "world", "abc"];
+// let x=arr.map((a)=>{
+//     return (a.toUpperCase()+"!")
+// })
+// console.log(x)
+
+
+
+// let array = ["hello", "world", "max"];
+// let x=array.find((a)=>{return a.includes("o")})
+// console.log(x)
+
+
+let array=["hello", "world", "abc"];
+console.log(array.join(","))
+
+
+// let arr=["hello-world", "abc-def"];
+// let x=arr.map((a)=>{return a.split("-")})
+// console.log(x)
