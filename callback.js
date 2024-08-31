75a233508dc118fb5010f5b40216e56d5bb29a44
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-08-31 11:39:10 +0530

    modified

--- callback.js
+++ callback.js
@@ -1,189 +1,265 @@
-// console.log("map")
+// // // console.log("map")
 
-let arr = [1, 2, 3, 4, 5];
-res = " ";
-arr.map((a) => {
-  res += a * 2;
-});
-console.log(res);
+// // let arr = [1, 2, 3, 4, 5];
+// // res = " ";
+// // arr.map((a) => {
+// //   res += a * 2;
+// // });
+// // console.log(res);
 
 
 
-let arr = ["hello", "world"];
-res = [];
-j = 0;
-arr.map((a) => {
-  for (i = 0; i < arr.length; i++) {
-    res[j] = arr[i].toUpperCase();
-    j++;
-  }
-});
-console.log(res);
+// // let arr = ["hello", "world"];
+// // res = [];
+// // j = 0;
+// // arr.map((a) => {
+// //   for (i = 0; i < arr.length; i++) {
+// //     res[j] = arr[i].toUpperCase();
+// //     j++;
+// //   }
+// // });
+// // console.log(res);
 
 
 
-// console.log("Filter");
+// // // console.log("Filter");
 
 
 
 
-let arr = [1, 2, 3, 4, 5];
-res = " ";
-arr.filter((a) => {
-  if (a % 2 == 0) {
-    res += a + " ";
-  }
-});
-console.log(res);
+// // let arr = [1, 2, 3, 4, 5];
+// // res = " ";
+// // arr.filter((a) => {
+// //   if (a % 2 == 0) {
+// //     res += a + " ";
+// //   }
+// // });
+// // console.log(res);
 
 
 
-let characters = ["hello", "world", "abc"];
-for (string of characters) {
-  for (i = 0; i < string.length; i++) {
-    if (string.length >= 5) {
-      console.log(string);
-      break;
-    }
-  }
-}
+// // let characters = ["hello", "world", "abc"];
+// // for (string of characters) {
+// //   for (i = 0; i < string.length; i++) {
+// //     if (string.length >= 5) {
+// //       console.log(string);
+// //       break;
+// //     }
+// //   }
+// // }
 
 
 
-let characters = ["hello", "world", "abc"];
-characters.filter((a) => {
-  if (a.length % characters.length !== 0) {
-    console.log(a);
-  }
-});
+// // let characters = ["hello", "world", "abc"];
+// // characters.filter((a) => {
+// //   if (a.length % characters.length !== 0) {
+// //     console.log(a);
+// //   }
+// // });
 
 
 
-let characters = ["hello", "world", "abc"];
-characters.filter((a) => {
-  if (a.length % 5 == 0) {
-    console.log(a);
-  }
-});
+// // let characters = ["hello", "world", "abc"];
+// // characters.filter((a) => {
+// //   if (a.length % 5 == 0) {
+// //     console.log(a);
+// //   }
+// // });
 
 
 
-// console.log("For each");
+// // // console.log("For each");
 
 
 
-let arr = [1, 2, 3, 4, 5];
-arr.forEach((a) => {
-  console.log(a);
-});
+// // let arr = [1, 2, 3, 4, 5];
+// // arr.forEach((a) => {
+// //   console.log(a);
+// // });
 
 
-let arr = ["hello", "world"];
-res = "";
-arr.forEach((a) => {
-  res += a.concat("!") + " ";
-});
-console.log(res);
+// // let arr = ["hello", "world"];
+// // res = "";
+// // arr.forEach((a) => {
+// //   res += a.concat("!") + " ";
+// // });
+// // console.log(res);
 
 
 
 
 
-// console.log("every");
+// // // console.log("every");
 
 
 
 
-let arr = [1, 2, 3, 4, 5];
-arr.every((a) => {
-  console.log(a >= -1);
-});
+// // let arr = [1, 2, 3, 4, 5];
+// // arr.every((a) => {
+// //   console.log(a >= -1);
+// // });
 
 
 
 
-let arr = ["hello", "world", "abc"];
-arr.every((a) => {
-  if (a.length >= 5) {
-    console.log(a ? "true" : "false");
-  }
-});
+// // let arr = ["hello", "world", "abc"];
+// // arr.every((a) => {
+// //   if (a.length >= 5) {
+// //     console.log(a ? "true" : "false");
+// //   }
+// // });
 
 
 
 
-// console.log("some");
+// // // console.log("some");
 
 
 
 
-let array = [1, 2, 3, 4, 5];
-let x = array.some((a) => {
-  if (a % 2 == 0) {
-    return a ? "true" : "false";
-  }
-});
-console.log(x);
+// // let array = [1, 2, 3, 4, 5];
+// // let x = array.some((a) => {
+// //   if (a % 2 == 0) {
+// //     return a ? "true" : "false";
+// //   }
+// // });
+// // console.log(x);
 
 
 
 
-let array = ["hello", "world", "max"];
-let z = array.some((a) => {
-  if (a.includes("x")) {
-    return a ? "true" : "false";
-  }
-});
-console.log(z);
+// // let array = ["hello", "world", "max"];
+// // let z = array.some((a) => {
+// //   if (a.includes("x")) {
+// //     return a ? "true" : "false";
+// //   }
+// // });
+// // console.log(z);
 
 
 
 
 
-// console.log("find");
+// // // console.log("find");
 
 
 
 
-let array = [1, 2, 3, 4, 5];
-let x = array.find((a) => {
-  if (a % 2 == 0) {
-    return a;
-  }
-});
-console.log(x);
+// // let array = [1, 2, 3, 4, 5];
+// // let x = array.find((a) => {
+// //   if (a % 2 == 0) {
+// //     return a;
+// //   }
+// // });
+// // console.log(x);
 
 
 
 
-let array = ["hello", "world", "max"];
-let z = array.find((a) => {
-  if (a.includes("x")) {
-    return a;
-  }
-});
-console.log(z);
+// // let array = ["hello", "world", "max"];
+// // let z = array.find((a) => {
+// //   if (a.includes("x")) {
+// //     return a;
+// //   }
+// // });
+// // console.log(z);
 
 
 
 
-// console.log("findindex");
+// // // console.log("findindex");
 
 
 
 
-let array = [1, 2, 3, 4, 5];
-let x = array.findIndex((a) => {
-  if (a % 2 == 0) {
-    return a;
-  }
-});
-console.log(x);
+// // let array = [1, 2, 3, 4, 5];
+// // let x = array.findIndex((a) => {
+// //   if (a % 2 == 0) {
+// //     return a;
+// //   }
+// // });
+// // console.log(x);
 
-let array = ["hello", "world", "max"];
-let z = array.findIndex((a) => {
-  if (a.includes("x")) {
-    return a;
-  }
-});
-console.log(z);
+// // let array = ["hello", "world", "max"];
+// // let z = array.findIndex((a) => {
+// //   if (a.includes("x")) {
+// //     return a;
+// //   }
+// // });
+// // console.log(z);
+
+
+
+
+
+// // ------> Objects <----
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { c: 3, d: 4 };
+
+// const obj=Object.assign(obj1,obj2)
+// console.log(obj)
+
+
+
+
+
+// const obj = { a: 1, b: 2, c: 3, d: 4 };
+// let x=Object.values(obj)
+// console.log(x)
+// let b=x.map((a)=>{return a})
+// console.log(b)
+// let c=b.reduce((a,b)=>{return a+b})
+// console.log("Sum up the values of obj's properties using reduce is " + c)
+
+
+
+
+// const obj = { a: 1, b: 2, c: 3, d: 4 }
+// newobj={};
+// Object.keys(obj).forEach(a=>{
+//     if(obj[a]>2){
+//         newobj[a]=obj[a]
+//     }
+// });
+// console.log(newobj)
+
+
+
+
+// ------>  date <----
+
+const d = new Date("2024-08-28");
+console.log(d)
+const x=d.setDate(d.getDate() + 3);
+const newc=new d.toDateString();
+console.log(newc)
+
+
+
+
+// const date=new Date();
+// const year=date.toDateString(date.getDate()+3);
+// console.log(year)
+// // const dayofweek=date.setTime(UTC+5:30);
+// // console.log(dayofweek)
+// // const months=date.setMonth(11);
+// // console.log(months)
+// // const dates=date.getDate();
+// // console.log(dates)
+// const week=new Date(date.getTime()+7*24*60*60*1000)
+// console.log(week)
+
+
+// const date1=new Date("2024-08-28")
+// const date2=new Date("2024-09-01")
+// const res=Math.abs(date1-date2);
+// const res1=24*60*60*1000
+// console.log(res,res1)
+// console.log(Math.floor(res/res1))
+
+
+
+// const news=new Date();
+//  news.setDate(news.getDate() +3);
+// console.log(news)
--- problem.js
+++ problem.js
@@ -1,306 +1,477 @@
-// // // // // // // // var a=50;
-// // // // // // // //     b=40;
-// // // // // // // //     c=30;
-// // // // // // // // if(a>b && a>c){
-// // // // // // // //     console.log("a is largest number");
-// // // // // // // // }
-// // // // // // // // else if(b>c){
-// // // // // // // //     console.log("b is largest number")
-// // // // // // // // }
-// // // // // // // // else{
-// // // // // // // //     console.log("c is largest number")
-// // // // // // // // }
+// // // // // // // // // var a=50;
+// // // // // // // // //     b=40;
+// // // // // // // // //     c=30;
+// // // // // // // // // if(a>b && a>c){
+// // // // // // // // //     console.log("a is largest number");
+// // // // // // // // // }
+// // // // // // // // // else if(b>c){
+// // // // // // // // //     console.log("b is largest number")
+// // // // // // // // // }
+// // // // // // // // // else{
+// // // // // // // // //     console.log("c is largest number")
+// // // // // // // // // }
 
-// // // // // // // // var a=50;
-// // // // // // // //     b=40;
-// // // // // // // //     c=30;
-// // // // // // // // result=(a>b && a>c)?"a is bigger number":(b>c)?"b is bigger number":"c is bigger number";
-// // // // // // // // console.log(result)
+// // // // // // // // // var a=50;
+// // // // // // // // //     b=40;
+// // // // // // // // //     c=30;
+// // // // // // // // // result=(a>b && a>c)?"a is bigger number":(b>c)?"b is bigger number":"c is bigger number";
+// // // // // // // // // console.log(result)
 
-// // // // // // // // for(i=1;!(i>=10+1);i++){
-// // // // // // // //     console.log(i)
-// // // // // // // // }
+// // // // // // // // // for(i=1;!(i>=10+1);i++){
+// // // // // // // // //     console.log(i)
+// // // // // // // // // }
 
-// // // // // // // // for(e=1;(e!=10+1);e++){
-// // // // // // // //     console.log(e)
-// // // // // // // // }
+// // // // // // // // // for(e=1;(e!=10+1);e++){
+// // // // // // // // //     console.log(e)
+// // // // // // // // // }
 
-// // // // // // // // for(i=10;(i>=0);i--){
-// // // // // // // //     console.log(i)
-// // // // // // // // }
+// // // // // // // // // for(i=10;(i>=0);i--){
+// // // // // // // // //     console.log(i)
+// // // // // // // // // }
 
-// // // // // // // // for(i=10;!(i<=0);i--){
-// // // // // // // //     console.log(i)
-// // // // // // // // }
+// // // // // // // // // for(i=10;!(i<=0);i--){
+// // // // // // // // //     console.log(i)
+// // // // // // // // // }
 
-// // // // // // // // ------> 13/08/2024 <-----
+// // // // // // // // // ------> 13/08/2024 <-----
+
+// // // // // // // // let rev="";
+// // // // // // // // let sum=1;
+// // // // // // // // function fac(n){
+// // // // // // // // for(i=1;i<=n;i++){
+// // // // // // // //     rev+=(i);
+// // // // // // // //     sum=sum*i
+// // // // // // // // }console.log(rev.split("").join("*")+" = "+sum)}
+// // // // // // // // fac(6)
 
 // // // // // // // let rev="";
-// // // // // // // let sum=1;
-// // // // // // // function fac(n){
-// // // // // // // for(i=1;i<=n;i++){
-// // // // // // //     rev+=(i);
-// // // // // // //     sum=sum*i
-// // // // // // // }console.log(rev.split("").join("*")+" = "+sum)}
-// // // // // // // fac(6)
+// // // // // // // let sum=0;
+// // // // // // // function prime(n){
+// // // // // // // for(x=1;x<=n;x++){
+// // // // // // //   if(n%x===0){
+// // // // // // //     rev=rev+[x]
+// // // // // // //     // console.log(rev)
+// // // // // // //   }
+// // // // // // //   else{
+// // // // // // //     console.log(x)
+// // // // // // //   }
 
-// // // // // // let rev="";
-// // // // // // let sum=0;
-// // // // // // function prime(n){
-// // // // // // for(x=1;x<=n;x++){
-// // // // // //   if(n%x===0){
-// // // // // //     rev=rev+[x]
-// // // // // //     // console.log(rev)
-// // // // // //   }
-// // // // // //   else{
-// // // // // //     console.log(x)
-// // // // // //   }
+// // // // // // // }
+// // // // // // //  if(rev.length==2){
+// // // // // // //      console.log(`${n} it is a prime number`)
+// // // // // // //  }
+// // // // // // //  else{
+// // // // // // //      console.log(`${n} it is not a prime number`)
+// // // // // // //  }}
+// // // // // // //  prime (5)
 
-// // // // // // }
-// // // // // //  if(rev.length==2){
-// // // // // //      console.log(`${n} it is a prime number`)
-// // // // // //  }
-// // // // // //  else{
-// // // // // //      console.log(`${n} it is not a prime number`)
-// // // // // //  }}
-// // // // // //  prime (5)
+// // // // // // // -------> 14/08/2024 <------
 
-// // // // // // -------> 14/08/2024 <------
-
-// // number=prompt("enter the value");
-// // non_primesum=0;
-// // primesum=0;
-// // function prime(number){
-// // for(num of number){
-// //        if(num>1){
-// //         count=0
-// //         for(i=2;i<num;i++){
-// //             if(num%i==0){
-// //                 non_primesum+=Number(num);
-// //                 count++;
-// //                 break;
-// //             }
-// //         }
-// //         if(count==0){
-// //             primesum+=Number(num);
-// //         }
-// //     }
-// //     else{
-// //         console.log("enter the valid number")
-// //     }
-// // }
-// //     console.log(non_primesum)
-// //     console.log(primesum)
-// // }
-
-// // num=prompt('enter the value')
-// // function prime(num){
-// //     if(num>1){
-// //     count=0;
-// //     for(i=2;i<num;i++){
-// //         if(num%i==0){
-// //             console.log(`${num} is a not prime number`);
-// //             count--;
-// //             break;
-// //         }
-// //         if(count==0){
-// //             console.log(`${num} is a prime number`)
-// //             break;
-// //         }
-// //     }
-// // }
-// //  else{
-// //     console.log(`${num} is a invalid number`)
-// //         }
-// // }
-// // prime(num)
-
-// // let input=prompt("enter the value")
-// // nonprimesum=0; primesum=0;
-// // for(num of input){
-// //   if(num>1){
-// //     c=0;
-// //     for(i=2;i<num;i++){
-// //       if(num%i==0){
-// //         nonprimesum=nonprimesum+Number(num);
-// //         c++;
-// //         break
-// //       }
-// //     }
-// //       if(c==0){
-// //         primesum=primesum+Number(num)
-// //       }
-
-// //   }
-// // }
-// // result=(nonprimesum>primesum)?"primesum is bigger":"nonprimesum is bigger";
-// // console.log(result)
-
-// // // ----->17/08/2024 <------
-
-// // // num=+prompt("enter a series number");
-// // // a=0;b=1;
-// // // i=1;rev=" ";
-// // // while(i<=num){
-// // //     if(i==num){
-// // //     rev+=a;}
-// // //     c=a+b;
-// // //     a=b;
-// // //     b=c;
-// // //     i++;
+// // // number=prompt("enter the value");
+// // // non_primesum=0;
+// // // primesum=0;
+// // // function prime(number){
+// // // for(num of number){
+// // //        if(num>1){
+// // //         count=0
+// // //         for(i=2;i<num;i++){
+// // //             if(num%i==0){
+// // //                 non_primesum+=Number(num);
+// // //                 count++;
+// // //                 break;
+// // //             }
+// // //         }
+// // //         if(count==0){
+// // //             primesum+=Number(num);
+// // //         }
+// // //     }
+// // //     else{
+// // //         console.log("enter the valid number")
+// // //     }
 // // // }
-// // // console.log(rev)
+// // //     console.log(non_primesum)
+// // //     console.log(primesum)
+// // // }
 
-// // // function reverseNumber(num) {
-// // //     return (
-// // //       parseFloat(
-// // //         num
-// // //           .toString()
-// // //           .split('')
-// // //           .reverse()
-// // //           .join('')
-// // //       )
-// // //       * Math.sign(num)
-// // //     )
+// // // num=prompt('enter the value')
+// // // function prime(num){
+// // //     if(num>1){
+// // //     count=0;
+// // //     for(i=2;i<num;i++){
+// // //         if(num%i==0){
+// // //             console.log(`${num} is a not prime number`);
+// // //             count--;
+// // //             break;
+// // //         }
+// // //         if(count==0){
+// // //             console.log(`${num} is a prime number`)
+// // //             break;
+// // //         }
+// // //     }
+// // // }
+// // //  else{
+// // //     console.log(`${num} is a invalid number`)
+// // //         }
+// // // }
+// // // prime(num)
+
+// // // let input=prompt("enter the value")
+// // // nonprimesum=0; primesum=0;
+// // // for(num of input){
+// // //   if(num>1){
+// // //     c=0;
+// // //     for(i=2;i<num;i++){
+// // //       if(num%i==0){
+// // //         nonprimesum=nonprimesum+Number(num);
+// // //         c++;
+// // //         break
+// // //       }
+// // //     }
+// // //       if(c==0){
+// // //         primesum=primesum+Number(num)
+// // //       }
+
 // // //   }
+// // // }
+// // // result=(nonprimesum>primesum)?"primesum is bigger":"nonprimesum is bigger";
+// // // console.log(result)
 
-// // //   console.log(reverseNumber("240"))
+// // // // ----->17/08/2024 <------
 
-// // // let num=+prompt("enter a number")
-// // // a=0;b=1;
-// // // i=0;rev="";
-// // // while(a<=num){
-// // //       rev+=a+" ";
-// // //       c=a+b;
-// // //       a=b;
-// // //       b=c;
-// // //       i++;
-// // //   }
-// // //   console.log(rev)
+// // // // num=+prompt("enter a series number");
+// // // // a=0;b=1;
+// // // // i=1;rev=" ";
+// // // // while(i<=num){
+// // // //     if(i==num){
+// // // //     rev+=a;}
+// // // //     c=a+b;
+// // // //     a=b;
+// // // //     b=c;
+// // // //     i++;
+// // // // }
+// // // // console.log(rev)
 
-// // -----> 20/08/2024 <-----
-// /
-// let num=+prompt("enter the number");
-// a=0;
-// b=1;
-// nonfib=0;
-// rev="";
-// x=0;
-// while(nonfib!=num){
-//     let i=a+1;
-//     while(i<b){
-//     rev+=i+" ";
-//     nonfib++;
-//     i++
-//       if(nonfib==num){
-//         break;
-//     }  }
-//    c=a+b;
-//     a=b;
-//     b=c;
-// }
-// console.log(rev)
+// // // // function reverseNumber(num) {
+// // // //     return (
+// // // //       parseFloat(
+// // // //         num
+// // // //           .toString()
+// // // //           .split('')
+// // // //           .reverse()
+// // // //           .join('')
+// // // //       )
+// // // //       * Math.sign(num)
+// // // //     )
+// // // //   }
 
-// let num=+prompt("enter the number");
-// a=0;
-// b=1;
-// nonfib=0;
-// rev="";
-// x=0;
-// while(nonfib!=num){
-//     let i=a+1;
-//     while(i<b){
-//     rev+=i+" ";
-//     x=i;
-//     nonfib++;
-//     i++
-//       if(nonfib==num){
-//         break;
-//     }  }
-//    c=a+b;
-//     a=b;
-//     b=c;
-// }
-// console.log(x)
+// // // //   console.log(reverseNumber("240"))
 
-// let num=+prompt("enter a number");
-// a=0;b=1;nonfib=0;rev="";+
-// 87g
-// while(nonfib!=num){
-//     for(i=a+1;i<b;i++){
-//         nonfib++;
-//         if(nonfib==num){
-//             break;
+// // // // let num=+prompt("enter a number")
+// // // // a=0;b=1;
+// // // // i=0;rev="";
+// // // // while(a<=num){
+// // // //       rev+=a+" ";
+// // // //       c=a+b;
+// // // //       a=b;
+// // // //       b=c;
+// // // //       i++;
+// // // //   }
+// // // //   console.log(rev)
+
+// // // -----> 20/08/2024 <-----
+// // /
+// // let num=+prompt("enter the number");
+// // a=0;
+// // b=1;
+// // nonfib=0;
+// // rev="";
+// // x=0;
+// // while(nonfib!=num){
+// //     let i=a+1;
+// //     while(i<b){
+// //     rev+=i+" ";
+// //     nonfib++;
+// //     i++
+// //       if(nonfib==num){
+// //         break;
+// //     }  }
+// //    c=a+b;
+// //     a=b;
+// //     b=c;
+// // }
+// // console.log(rev)
+
+// // let num=+prompt("enter the number");
+// // a=0;
+// // b=1;
+// // nonfib=0;
+// // rev="";
+// // x=0;
+// // while(nonfib!=num){
+// //     let i=a+1;
+// //     while(i<b){
+// //     rev+=i+" ";
+// //     x=i;
+// //     nonfib++;
+// //     i++
+// //       if(nonfib==num){
+// //         break;
+// //     }  }
+// //    c=a+b;
+// //     a=b;
+// //     b=c;
+// // }
+// // console.log(x)
+
+// // let num=+prompt("enter a number");
+// // a=0;b=1;nonfib=0;rev="";+
+// // 87g
+// // while(nonfib!=num){
+// //     for(i=a+1;i<b;i++){
+// //         nonfib++;
+// //         if(nonfib==num){
+// //             break;
+// //         }
+// //          if(i<=num){
+// //        rev+=i+" "
+// //     }
+// //     }
+
+// //     c=a+b;
+// //     a=b;
+// //     b=c;
+// // }console.log(rev)
+
+
+
+
+// // ---->26/08/2024     (unq and dupl number)  s<-----
+
+
+// arr=[5,6,8,4,9,4,1,8,8,8,7,5,2,4]
+// emp=[];uni="";dup=""
+// for(i of arr){
+//     if(!emp.includes(i)){
+//         emp.push(i)
+//     }
+// }console.log(emp);
+// for(i of emp){
+//     h=1;
+//     for(j of arr){
+//         if(i==j){
+//            h++
 //         }
-//          if(i<=num){
-//        rev+=i+" "
 //     }
+//     if(h==2){
+//         uni+=i+" "
 //     }
-
-//     c=a+b;
-//     a=b;
-//     b=c;
-// }console.log(rev)
-
-
-
-
-// ---->26/08/2024     (unq and dupl number)  s<-----
+//     else{
+//         dup+=i+" "
+//     }
+// }
+// let res1=(uni.length<=1)?"uniques number is "+uni: "uniques number are " +uni 
+// let res=(dup.length<=1)?"duplicates number is "+dup: "duplicates number are " +dup 
+// console.log(res1)
 
 
-arr=[5,6,8,4,9,4,1,8,8,8,7,5,2,4]
-emp=[];uni="";dup=""
-for(i of arr){
-    if(!emp.includes(i)){
-        emp.push(i)
-    }
-}console.log(emp);
-for(i of emp){
-    h=1;
-    for(j of arr){
-        if(i==j){
-           h++
-        }
-    }
-    if(h==2){
-        uni+=i+" "
-    }
-    else{
-        dup+=i+" "
-    }
+
+
+
+// arr=[5,6,8,4,9,4,1,8,8,8,7,5,2,4]
+// emp=[];uni="";dup=""
+// for(i of arr){
+//     if(!emp.includes(i)){
+//         emp.push(i)
+//     }
+// }console.log(emp);
+// for(i of emp){
+//     h=0;
+//     for(j of arr){
+//         if(i==j){
+//            h++
+//         }
+//     }
+//     if(h==1){
+//         uni+=i+" "
+//         console.log(`${i} -> ${h}`)
+//     }
+//     else{
+//         dup+=i+" "
+//         console.log(`${i} -> ${h}`)
+//     }
+// }
+// let res1=(uni.length<=1)?"uniques number is "+uni: "uniques number are " +uni 
+// let res=(dup.length<=1)?"duplicates number is "+dup: "duplicates number are " +dup 
+// console.log(res1)
+// console.log(res)
+
+
+// // ---->  duplicat <---
+
+
+// arr=[23,33,200,785];
+// uni="";dup=""
+// for(i of arr){
+// x=String(i).split("").map(Number)
+// emp=[]
+// for(g of x){
+//     if(!emp.includes(g)){
+//         emp.push(g)
+//     }
+    
+// }
+// // console.log(emp)
+// for(i of emp){
+//     c=0;
+//     for(j of x){
+//         if(i==j){
+//             c++
+//         }
+//     }
+//     if(c==1){
+//         uni+=i
+//     }
+//     else{
+//         dup+=i
+//     }
+// }
+
+// }
+// console.log("1) WAP to print duplicate from given number and concat the value")
+// console.log(dup)
+
+
+
+// console.log(dup)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arr=[8,2,4,1,2,1,5];
+// uni="";dup=""
+// const duplicates = arr.filter((item, index) => arr.indexOf(item) == index);
+// for(i of duplicates){
+//     c=0;
+//     for(j of arr){
+//         if (i==j){
+//             c++
+//         }
+//     }
+//     if(c==1){
+//         uni+=i+" "
+//     }
+//     else{
+//         dup+=i+" "
+//     }
+// }
+// console.log(uni);
+
+
+
+
+
+// // ------>tcs not <--------
+
+
+
+// Nums=[1,2,-1,1,3,1]
+// emp=[];nrep=""
+// for(i of Nums){
+//     if(!emp.includes(i)){
+//         emp.push(i)
+//     }
+// }
+// for(i of emp){
+//     c=0;
+//     for(j of Nums){
+//         if(i==j){
+//             c++
+//         }
+//     }
+//     if(c==1){
+//        nrep+=i+", "
+//     }
+    
+// }
+// console.log(nrep)
+
+
+
+
+
+
+// num="871659"
+// n=num.split("").map(Number).sort();
+// c=n.slice(0,3)
+// d=n.slice(3,6).sort((a,b)=>b-a)
+// console.log(c.concat(d))
+
+
+
+
+// num="87165945"
+// temp=[]
+// n=num.split("").map(Number);
+// for(i of n){
+//     temp.push(i)
+// }
+// temp.sort((a,b)=>a-b)
+// console.log(temp)
+// lengths=temp.length
+// mid=Math.ceil(lengths/2)
+// fh=temp.slice(0,mid)
+// sh=temp.slice(mid).sort((a,b)=>b-a)
+// res=fh.concat(sh)
+// console.log(res.flat())
+
+
+
+// -------> WAP to print number of pairs formed by the array of numbers <-------
+
+
+
+
+let arr="30 50 30 50 20 50 50 20 50 50 50"
+let num=arr.split(" ").map(Number);
+temp=[];res="";
+sum=0;
+for(i of num){7
+   if(!temp.includes(i)){
+   temp.push(i)
 }
-let res1=(uni.length<=1)?"uniques number is "+uni: "uniques number are " +uni 
-let res=(dup.length<=1)?"duplicates number is "+dup: "duplicates number are " +dup 
-console.log(res1)
-
-
-
-
-
-arr=[5,6,8,4,9,4,1,8,8,8,7,5,2,4]
-emp=[];uni="";dup=""
-for(i of arr){
-    if(!emp.includes(i)){
-        emp.push(i)
-    }
-}console.log(emp);
-for(i of emp){
-    h=0;
-    for(j of arr){
-        if(i==j){
-           h++
-        }
-    }
-    if(h==1){
-        uni+=i+" "
-        console.log(`${i} -> ${h}`)
-    }
-    else{
-        dup+=i+" "
-        console.log(`${i} -> ${h}`)
-    }
 }
-let res1=(uni.length<=1)?"uniques number is "+uni: "uniques number are " +uni 
-let res=(dup.length<=1)?"duplicates number is "+dup: "duplicates number are " +dup 
-console.log(res1)
-console.log(res)
+console.log(temp)
+for(i of temp){
+    c=0;
+    for(j of num){
+        
+        if(i==j){
+            c++
+    
+        }
+      
+    }
+    res+=Math.floor(c/2)
+}
+
+for(i of res){
+   sum+=Number(i)
+}
+console.log(" Sum of pair count of "+arr + "is " +sum)
+ 
