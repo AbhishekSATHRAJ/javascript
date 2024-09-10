02ca1906ce181146cb0c303ff8746d274299e607
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-09-10 22:32:40 +0530

    modified

--- callback.js
+++ callback.js
@@ -194,46 +194,46 @@
 
 // // ------> Objects <----
 
-// const obj1 = { a: 1, b: 2 };
-// const obj2 = { c: 3, d: 4 };
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
 
-// const obj=Object.assign(obj1,obj2)
-// console.log(obj)
-
-
-
-
-
-// const obj = { a: 1, b: 2, c: 3, d: 4 };
-// let x=Object.values(obj)
-// console.log(x)
-// let b=x.map((a)=>{return a})
-// console.log(b)
-// let c=b.reduce((a,b)=>{return a+b})
-// console.log("Sum up the values of obj's properties using reduce is " + c)
+const obj=Object.assign(obj1,obj2)
+console.log(obj)
 
 
 
 
-// const obj = { a: 1, b: 2, c: 3, d: 4 }
-// newobj={};
-// Object.keys(obj).forEach(a=>{
-//     if(obj[a]>2){
-//         newobj[a]=obj[a]
-//     }
-// });
-// console.log(newobj)
+
+const obj = { a: 1, b: 2, c: 3, d: 4 };
+let x=Object.values(obj)
+console.log(x)
+let b=x.map((a)=>{return a})
+console.log(b)
+let c=b.reduce((a,b)=>{return a+b})
+console.log("Sum up the values of obj's properties using reduce is " + c)
+
+
+
+
+const obj = { a: 1, b: 2, c: 3, d: 4 }
+newobj={};
+Object.keys(obj).forEach(a=>{
+    if(obj[a]>2){
+        newobj[a]=obj[a]
+    }
+});
+console.log(newobj)
 
 
 
 
 // ------>  date <----
 
-const d = new Date("2024-08-28");
-console.log(d)
-const x=d.setDate(d.getDate() + 3);
-const newc=new d.toDateString();
-console.log(newc)
+// const d = new Date("2024-08-28");
+// console.log(d)
+// const x=d.setDate(d.getDate() + 3);
+// const newc=new d.toDateString();
+// console.log(newc)
 
 
 
